6a817a40f2ab90a971b2bbc56400f87ccca3e00f
Daniel_Casey <dcasey07.yahoo.com>
2023-11-14 21:10:22 -0500

    Updated jupyter notebook and starter flask code

--- app.py
+++ app.py
@@ -1,30 +1,68 @@
 # Import the dependencies.
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
+Base.prepare(autoload_with=engine)
 
-
-# Save references to each table
-
-
-# Create our session (link) from Python to the DB
-
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 #################################################
 # Flask Setup
 #################################################
-
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/<br/>"
+        f"/api/v1.0/precipitation"
+        f"/api/v1.0/stations"
+        f"/api/v1.0/tobs"
+        f"/api/v1.0/<start>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Design a query to retrieve the last 12 months of precipitation data and plot the results. 
+    # Starting from the most recent data point in the database. 
+    most_recent = '2017-08-23'
+    # Calculate the date one year from the last date in data set.
+    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    # Perform a query to retrieve the data and precipitation scores
+    one_year_precip = session.query(Measurement.date, Measurement.prcp).\
+                        filter(Measurement.date >= year_ago).\
+                        order_by(Measurement.date).all()
+    # Convert the query to a list of dictionaries
+    one_year_precip_dict = [{'date': date, 'prcp': prcp} for date, prcp in one_year_precip]
+    return jsonify(one_year_precip_dict)
+
+if __name__ == '__main__':
+    app.run(debug=True)
